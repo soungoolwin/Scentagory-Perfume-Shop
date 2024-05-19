93e39fd050209a1ec5e1d7c38aca9c622269e0b8
Soung Oo Lwin <soungoolwin275@gmail.com>
2024-05-19 23:23:10 +0700

    Environment setup

---
+++ resources/js/Components/Hero.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>This is hero section</h1>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <app-layout>
+        <template #default>
+            <hero />
+        </template>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Hero from "../Components/Hero.vue";
+
+export default {
+    props: {},
+    components: {
+        AppLayout,
+        Hero,
+    },
+};
+</script>
